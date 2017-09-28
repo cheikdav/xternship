d51a981d9a993bc045ac295170db8df439c6af44
David <david@David.localdomain>
2017-09-28 23:51:32 +0200

    Ajout du form et mise en place de l'action du bouton

--- proposition/forms.py
+++ proposition/forms.py
@@ -5,16 +5,14 @@ class Type_Recherche(forms.Form):
     choix = ((0,'PSC'), (1, '1A'),(2,'2A'))
     print(choix)
     type_stage = forms.MultipleChoiceField(label = '', choices = choix , widget=forms.CheckboxSelectMultiple(), initial = [c[0] for c in choix] )
+    cat_voulues = forms.ModelMultipleChoiceField(label = 'Categories', queryset = Categorie.objects.all(), widget=forms.CheckboxSelectMultiple(), initial = Categorie.objects.all()) 
+
     def clean_my_field(self):
         if len(self.cleaned_data['type_stage']) == 0:
             raise forms.ValidationError('Choisir au moins un type de proposition')
-        return self.cleaned_data['type_stage']
-    
-class Categorie_Recherche(forms.Form):
-    cat_wanted = forms.ModelMultipleChoiceField(label = 'Categories', queryset = Categorie.objects.all(), widget=forms.CheckboxSelectMultiple(), initial = Categorie.objects.all()) 
-    def clean_my_field(self):
-        if len(self.cleaned_data['cat_wanted']) == 0:
+	if len(self.cleaned_data['cat_voulues']) == 0:
             raise forms.ValidationError('Choisir au moins une categorie')
-        return self.cleaned_data['cat_wanted']
+        return super(Type_Recherche, self).clean()
+    
 
 	
--- proposition/views.py
+++ proposition/views.py
@@ -5,21 +5,23 @@ from django.shortcuts import render, get_object_or_404
 from .models import Proposition
 from django.http import HttpResponse
 from django.template import loader
-from .forms import Type_Recherche, Categorie_Recherche
+from .forms import Type_Recherche
 
 # Create your views here.
 def index(request):
     
     if request.method == "POST":
 	type_rech = Type_Recherche(request.POST)
-	cat_rech = Categorie_Recherche(request.POST)
+	if type_rech.is_valid():
+	    types_stages = type_rech.cleaned_data.get('type_stage')
+	    categories_voulues = type_rech.cleaned_data.get('cat_voulues')
+	    
     else:
 	type_rech = Type_Recherche()
-	cat_rech = Categorie_Recherche()
     
     latest_propositions = Proposition.objects.order_by('-date_publication')[:5]
     
-    return render(request, 'proposition/index.html', {'latest_proposition': latest_propositions, 'type_rech' : type_rech, 'cat_rech' : cat_rech})
+    return render(request, 'proposition/index.html', {'latest_proposition': latest_propositions, 'type_rech' : type_rech})
 
 def detail_prop(request, prop_id):
     prop = get_object_or_404(Proposition,pk= prop_id)
